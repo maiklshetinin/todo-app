5f0e6666859f0642b7b956ec36ad35639fbe209c
maiklshetinin <sh_chetinin@mail.ru>
2024-11-05 23:10:50 +0300

    feat: add MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import Dialog from "./components/Dialog.vue";
+import MainPage from "./components/MainPage.vue";
+
+const showRegisterDialog = ref(true);
+const showLoginDialog = ref(false);
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <MainPage />
+  </main>
+
+  <!-- <Dialog v-model="showRegisterDialog"> Registration </Dialog>
+  <Dialog v-model="showLoginDialog"> Login </Dialog> -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import ru from "element-plus/dist/locale/ru.mjs";
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(ElementPlus, {
+    locale: ru,
+  })
+  .mount("#app");
